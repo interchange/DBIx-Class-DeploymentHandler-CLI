bf8b259fa2195227f4bd8a664e0f789d333ddc40
Stefan Hornburg (Racke) <racke@linuxia.de>
2017-04-29 11:21:54 +0200

    Fix missing slash in home config path.

--- lib/DBIx/Class/DeploymentHandler/CLI/ConfigReader.pm
+++ lib/DBIx/Class/DeploymentHandler/CLI/ConfigReader.pm
@@ -55,7 +55,7 @@ sub _builder_config_paths {
     return [
         $self->get_env_vars,
         ".$config_name",
-        File::HomeDir->my_home . $config_name,
+        File::HomeDir->my_home . '/' . $config_name,
         "/etc/$config_name",
     ];
 }
